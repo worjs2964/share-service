c24478aab08ea8b32c0fada60f966899f39e3c78
JAEGEON LEE <worjs2964@gmail.com>
2021-12-29 22:36:08 +0900

    initial commit

---
+++ src/main/java/com/project/ottshareservice/OttShareServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.ottshareservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OttShareServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OttShareServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/ottshareservice/OttShareServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ottshareservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OttShareServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
